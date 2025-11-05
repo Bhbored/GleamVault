fbfb6606980247730bf1358c39a2bd4e85dfd662
Bhbored <bhbored2022@gmail.com>
2025-11-05 13:56:08 +0200

    aaa

--- Shared/Models/TransactionItem.cs
+++ Shared/Models/TransactionItem.cs
@@ -12,6 +12,8 @@ namespace Shared.Models
         public Guid TransactionId { get; set; }
         public Guid ProductId { get; set; }   
         public int Quantity { get; set; }
-        public decimal UnitPriceAtSale { get; set; }
+        public decimal UnitPrice { get; set; } = 0;
+        public decimal UnitCost { get; set; } = 0;
+
     }
 }
