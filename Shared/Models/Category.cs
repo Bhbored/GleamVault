84b8f1b43b6d7b77fb26cfed76cef26d42e62b8b
awnar jundi <anwarioanan@gmail.com>
2025-11-05 13:47:53 +0200

    updated shared

---
+++ Shared/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Category
+    {
+      
+        public Guid Id { get; set; }
+       
+        public string Name { get; set; }
+
+      
+        public string? Description { get; set; }
+
+
+
+        public ICollection<Product> Product { get; set; }
+    }
+}
---
+++ Shared/Models/TransactionItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class TransactionItem
+    {
+      
+        public Guid Id { get; set; }
+
+     
+        public Guid TransactionId { get; set; }
+      
+        public Guid ProductId { get; set; }
+        
+        public int Quantity { get; set; }
+     
+        public float UnitPriceAtSale { get; set; }
+      
+        public float UnitCostAtSale { get; set; }
+
+
+        public virtual Product Product { get; set; }
+        public virtual Transaction Transaction { get; set; }
+    }
+}
