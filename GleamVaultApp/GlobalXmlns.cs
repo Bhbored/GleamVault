a9d46fe3d0d8949a5388ab14396979b5c3286d94
Bhbored <bhbored2022@gmail.com>
2025-11-11 16:13:08 +0200

    fixed the loading issue

---
+++ GleamVaultApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "GleamVaultApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "GleamVaultApp.Pages")]
---
+++ GleamVaultApp/MauiProgram.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Maui;
+using GleamVault.Services;
+using GleamVault.Services.Interfaces;
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Syncfusion.Maui.Core.Hosting;
+using Syncfusion.Maui.Toolkit.Hosting;
+namespace GleamVaultApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .ConfigureSyncfusionToolkit()
+                .ConfigureSyncfusionCore()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Poppins-Light.ttf", "NexaLight");
+                    fonts.AddFont("Poppins-Bold.ttf", "NexaHeavy");
+                });
+
+
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+            builder.Services.RegisterDependencies();
+            return builder.Build();
+        }
+    }
+}
